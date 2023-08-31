9077b9da346d257ca556dffe6613429924ea74ed
550tealeaves <nastasia.claire89@gmail.com>
2023-08-31 19:22:20 -0400

    added 1st lesson

---
+++ Lecture 1/script.js
@@ -0,0 +1,15 @@
+var book, author, isbn, description;
+
+book = "Hunger";
+author = "Knut Hamsun";
+isbn = 0374525285;
+
+description = "The book " + book + " by " + author + " is on ISBN " + isbn;
+console.log(description);
+
+book = "The Master and Margarita";
+author = "Mikhail Bulgakov";
+isbn = 0679760806;
+
+description = "The book " + book + " by " + author + " is on ISBN " + isbn;
+console.log(description);
