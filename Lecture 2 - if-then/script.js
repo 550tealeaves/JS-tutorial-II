c776a88a8b3d611062ac559108fc4c330505ca52
550tealeaves <nastasia.claire89@gmail.com>
2023-09-01 12:15:12 -0400

    updated lessons

--- Lecture 2 - if-then/script.js
+++ Lecture 2 - if-then/script.js
@@ -1,4 +1,20 @@
+//Prompt() - returns inputs as strings
 var userName = prompt("What is your name ? ");
-userName
+userName //will display the response entered into prompt
 
 var whatAmI = prompt("What am I", "I am human"); //I am human shows as response
+
+
+//parseInt() - this converts # from string to number - but cannot convert #s written as text (ex: five) - will return NaN (not a #)
+var userAge = prompt("How old are you?")
+userAge = parseInt(userAge) //take the response entered into the prompt & convert it to #
+alert("You are " + userAge + " years old") //Will show an alert w/ user input converted into #, w/in statement
+typeof userAge; //return integer
+
+
+// IF/ELSE statement - lets you react to user input
+if (condition) {
+    //code runs if condition is TRUE
+} else {
+    //code runs if condition is FALSE
+}
