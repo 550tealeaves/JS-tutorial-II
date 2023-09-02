d0b291035e8624d178926d4de26102d541074c1e
550tealeaves <nastasia.claire89@gmail.com>
2023-09-01 20:08:37 -0400

    changed isbn numbers to remove error

--- Lecture 1 - data types/script.js
+++ Lecture 1 - data types/script.js
@@ -2,14 +2,14 @@ var book, author, isbn, description; //can list all variables in 1 statement
 
 book = "Hunger";
 author = "Knut Hamsun";
-isbn = 0374525285;
+isbn = 374525285;
 
 description = "The book " + book + " by " + author + " is on ISBN " + isbn;
 console.log(description);
 
 book = "The Master and Margarita";
 author = "Mikhail Bulgakov";
-isbn = 0679760806;
+isbn = 679760806;
 
 description = "The book " + book + " by " + author + " is on ISBN " + isbn;
 console.log(description);
