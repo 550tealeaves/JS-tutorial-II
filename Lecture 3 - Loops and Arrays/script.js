0ec813157c5134ebeb5ee1bac49377458a27d222
550tealeaves <nastasia.claire89@gmail.com>
2023-09-09 13:16:32 -0400

    started classnotes

---
+++ Lecture 3 - Loops and Arrays/script.js
@@ -0,0 +1,7 @@
+//arrays can store a wide variety of data types and mixed types
+var myArray1 = [1, 2, 3, 4, 5];
+var myArray2 = ['a', 'b', 'c', 'd', 'e'];
+var myArray3 = [true, false, true, false]; 
+var myArray4 = [1, 2, 'dog', true, [1, 2, 3]]; 
+alert(myArray4[0]); // returns 1
+alert(myArray4[4]); //returns 1,2,3
