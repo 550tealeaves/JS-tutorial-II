ad5bc1af18fc995ab2cafd9820de36b6ebf6061a
550tealeaves <nastasia.claire89@gmail.com>
2023-09-16 14:54:40 -0400

    adding notes

---
+++ Lecture 4 - Functions/script.js
@@ -0,0 +1,8 @@
+//WHAT IS A FUNCTION?
+//Code that performs specific task and is reusable that can automate tasks
+//HOW TO CREATE A FUNCTION
+// 1. Define function name
+// 2. Use lower camelCase for name
+// 3. Define parameters or arguments - parameters = values passed into function when called = can reuse same function for diff tasks & you can pass diff values
+// 4. Must return a value - takes input, transforms it, returns output
+
