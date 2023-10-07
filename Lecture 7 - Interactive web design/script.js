f80973a969d00a63d35a0e00f1f1dd974d5d9b8b
550tealeaves <nastasia.claire89@gmail.com>
2023-10-06 23:32:54 -0400

    added more notes

--- Lecture 7 - Interactive web design/script.js
+++ Lecture 7 - Interactive web design/script.js
@@ -1,1 +1,13 @@
 alert("This project is going to be super duper awesome!");
+
+//Hide h1 - $("argument (element)")
+//$('h1').hide(); //hides all h1 elements - .hide() is a method
+
+//Selecting an ID - lets us target a single element
+$("#noShow").hide(); //will hide the element w/ ID "noshow"
+
+//Changing the text in hTML - use .html() method
+$("h1").html("The Best Jokes Ever!"); //will change ALL h1 elements to the new text
+
+
+
