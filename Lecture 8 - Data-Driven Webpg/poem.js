bb038077ee9129adc68acc237863247b4554ee72
550tealeaves <nastasia.claire89@gmail.com>
2023-10-13 23:15:13 -0400

    starting JS portion of data driven websites

---
+++ Lecture 8 - Data-Driven Webpg/poem.js
@@ -0,0 +1,20 @@
+//1st line of poem broken up into words 
+//Each word = object w/ text property that designates each word in poem
+let line1 = [{
+    text: "What"
+}, {
+    text: "hurrying"
+}, {
+    text: "human"
+}, {
+    text: 'tides'
+}, {
+    text: 'or'
+}, {
+    text: 'day'
+}, {
+    text: 'or'
+}, {
+    text: 'night!'
+}
+];
