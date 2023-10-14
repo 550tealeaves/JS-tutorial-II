9b30204cdb8b41725a710f9c553e4a1ab0aefb73
550tealeaves <nastasia.claire89@gmail.com>
2023-10-14 17:33:53 -0400

    updating notes

--- Lecture 8 - Data-Driven Webpg/poem.js
+++ Lecture 8 - Data-Driven Webpg/poem.js
@@ -1,6 +1,11 @@
+//Target ID info and add paragraph content to html
+$("#info").html("<p>The info will go here</p>"); 
+
+let line1, line1Text; //declaring variables 
+
 //1st line of poem broken up into words 
 //Each word = object w/ text property that designates each word in poem
-let line1 = [{
+line1 = [{
     text: "What"
 }, {
     text: "hurrying"
@@ -18,3 +23,37 @@ let line1 = [{
     text: 'night!'
 }
 ];
+
+//Use .map() to build a new array out of another array
+//.join() returns array as string
+line1Text = line1.map(function (word) {
+    return word.text;
+}).join(" "); 
+$("#poem").html("<p>" + line1Text + "<br /></p>"); //target ID poem and add paragraph containing the returned word array spaced out
+
+
+
+
+
+//.MAP() METHOD
+//Use .MAP() to iterate over each element and apply a function - creates new array w/o changing original
+//Use .map() to build a new array out of another array
+
+let testTextArray = [] //set it to empty array
+testTextArray = line1.map(function(word){ //test code
+    return word.text;
+})
+
+
+//.map() creates new array out of array of word objects. New array has only .text property (the words) of each object
+
+//.JOIN() METHOD - returns array as string
+//This example will return the array of words as string, separated by space
+let testText = testTextArray.join(" "); //this is test code
+
+
+
+
+
+
+
