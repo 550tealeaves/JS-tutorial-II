7a3fa9a1bc4c77ab042f9a0b737b18c10ed84e4a
550tealeaves <nastasia.claire89@gmail.com>
2023-11-03 16:42:18 -0400

    started the chapter and adding notes

---
+++ Lecture 11 - Geospatial Data/nyc-data.js
@@ -0,0 +1,17 @@
+let nyc = {}; //create new variable to store the JSON data
+//Call the map
+let nycMap;
+nycMap = L.map("map");
+
+//Create the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    maxZoom: 18,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(nycMap);
+//Can chain the different methods - could do L.tilelayer(.....{}).addTo(nycMap).setView([lat, lng], zoom);
+
+
+nycMap.setView([40.7128, -74.0060], 16);
+
+//Add data
+L.geoJSON(nyc).addTo(nycMap);
