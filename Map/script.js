103e2bbdc1cc5986df9d80ea2e6be47b9267fd03
550tealeaves <nastasia.claire89@gmail.com>
2023-12-14 18:04:20 -0500

    edited attestation

--- Map/script.js
+++ Map/script.js
@@ -5,7 +5,7 @@ museumMap = L.map("map");
 //Create the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors and <a href = "https://data.cityofnewyork.us/Recreation/New-York-City-Museums/ekax-ky3z">NYC Open Data</a>'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> & <a href ="https://data.cityofnewyork.us/Recreation/New-York-City-Museums/ekax-ky3z">NYC Open Data</a>'
 }).addTo(museumMap);
 console.log('museums', museums);
 //Can chain the different methods - could do L.tilelayer(.....{}).addTo(museumMap).setView([lat, lng], zoom);
