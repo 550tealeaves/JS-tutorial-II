9a9796049d0c9895a7b5cbac06b8ef8a4622963c
550tealeaves <nastasia.claire89@gmail.com>
2023-12-14 02:29:00 -0500

    still trying to update map

--- Map/script.js
+++ Map/script.js
@@ -19,21 +19,19 @@ L.geoJSON(museums, {
     
     //Challenge (3) - Change color of Manhattan to red - error feature is undefined
     style: function (feature) {
-        
-        
-        // if (feature.properties.city === "Manhattan") { //if city = Manhattan, outline, add color & transparency
-        //     return {
-        //         color: "red",
-        //         fillColor: "gold",
-        //         fillOpacity: 0.4
-        //     };
-        // } else { //if not Manhattan, then outline, color, and transparency
-        //     return {
-        //         color: 'indigo',
-        //         fillColor: 'pink',
-        //         fillOpacity: 0.5
-        //     };
-        // }
+        if (updateMap = true) { //if city = Manhattan, outline, add color & transparency
+            return {
+                color: "red",
+                fillColor: "gold",
+                fillOpacity: 0.4
+            };
+        } else { //if not Manhattan, then outline, color, and transparency
+            return {
+                color: 'indigo',
+                fillColor: 'pink',
+                fillOpacity: 0.5
+            };
+        }
 
     },
     //ADD POPUPS TO SHOW url/city NAMES
@@ -45,15 +43,6 @@ L.geoJSON(museums, {
     //(6) Add target='_blank' so link opens in new tab
     onEachFeature: function (feature, layer) {
         let link = feature.properties.url; //store path to urls in variable
-        // layer.on('mouseover', function(){
-        //     $('#map').append("<h3>" + feature.properties.name + "</h3> <hr> <a href=" + link + " + target='_blank' " + ">" + link + "</a>");
-        // }) //tried to create way to mouseover and show the name and link but didn't work
-        // $('#map').hover(function (){
-        //     $('#info').show(feature.properties.summary);
-        // },
-        // function(){
-        //     $('#info').hide(feature.properties.summary);
-        // });
         layer.bindPopup("<h3>" + feature.properties.name + "</h3> <hr> <a href=" + link + " + target='_blank' " + ">" + link + "</a>");
         //Add the summary of the places 
         layer.on('mouseover', function () { //will show the summary when you mouseover icon
@@ -165,6 +154,47 @@ $(document).ready(function (event) {
         console.log('borough', borough); //shows borough selected in console
     });
 });
+
+
+function filter(feature, layer) {
+    if (feature.properties) {
+        if (feature.properties.city === normalizedPickBorough) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+//RE-LOAD THE DATA TO ONLY INCLUDE THE FEATURES FROM THE DROPDOWN SELECTION
+//not working - and stops the summary and the popup from showing
+// var includeUpdateMap = false;
+
+// function filter(feature, layer) {
+//     if (feature.properties) {
+//         if (feature.properties.city === undefined || feature.properties.city === includeUpdateMap) {
+//             return false;
+//         } else {
+//             return true;
+//         }
+//     }
+//     return true;
+// }
+
+// function mapStyle(feature) {
+//     return {
+//         stroke: filter(feature)
+//     };
+// }
+
+// const layer = L.geoJSON(museums, {
+//     style: mapStyle
+// }).addTo(museumMap);
+
+// // Change style
+// includeUpdateMap = true;
+// layer.setStyle(mapStyle);
+
 
 //STYLE THE H1
 $("h1").hide(500).delay(1500).show(300).css({
